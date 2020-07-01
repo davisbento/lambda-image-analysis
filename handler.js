4c398f37d60fb65cf184cad39d860ac4cc44e641
davisbento <davisilva4222@gmail.com>
2020-07-01 15:41:40 -0300

    request.json

--- handler.js
+++ handler.js
@@ -1,13 +1,10 @@
 'use strict';
-
-const { promises: { readFile } } = require('fs')
-
+const { get } = require('axios')
 class Handler {
   constructor({ rekoSvc, translatorSvc }) {
     this.rekoSvc = rekoSvc
     this.translatorSvc = translatorSvc
   }
-
   async detectImageLabels(buffer) {
     const result = await this.rekoSvc.detectLabels({
       Image: {
@@ -15,13 +12,14 @@ class Handler {
       }
     }).promise()
 
+    const workingItems = result.Labels
+      .filter(({ Confidence }) => Confidence > 80);
 
-    const workingItems = result.Labels.filter(({ Confidence }) => Confidence > 90);
-
-    const names = workingItems.map(({ Name }) => Name).join(' and ')
-
+    const names = workingItems
+      .map(({ Name }) => Name)
+      .join(' and ')
     return { names, workingItems }
-  } 
+  }
 
   async translateText(text) {
     const params = {
@@ -29,57 +27,69 @@ class Handler {
       TargetLanguageCode: 'pt',
       Text: text
     }
-
-    const { TranslatedText } = await this.translatorSvc.translateText(params).promise()
-
+    const { TranslatedText } = await this.translatorSvc
+                            .translateText(params)
+                            .promise()
     return TranslatedText.split(' e ')
   }
 
   formatTextResults(texts, workingItems) {
+    
     const finalText = []
     for(const indexText in texts) {
-      const nameInPortuguesse = texts[indexText]
-      const confidence = workingItems[indexText].Confidence
-      finalText.push(`${confidence.toFixed(2)}% de ser ${nameInPortuguesse}`)
+      const nameInPortuguese = texts[indexText]
+      const confidence = workingItems[indexText].Confidence;
+      finalText.push(
+        ` ${confidence.toFixed(2)}% de ser do tipo ${nameInPortuguese}`
+
+      )
     }
-
-    return finalText;
+    return finalText.join('\n')
   }
-
+  async getImageBuffer(imageUrl) {
+    const response = await get(imageUrl, {
+      responseType: 'arraybuffer'
+    })
+    const buffer = Buffer.from(response.data, 'base64')
+    return buffer
+  }
   async main(event) {
     try {
-      const imgBuffer = await readFile('./images/cat.jpeg')
-      console.log('Detecting label...')
-
-      const { names, workingItems } = await this.detectImageLabels(imgBuffer)
-
+      const { imageUrl } = event.queryStringParameters
+      
+      // const imgBuffer = await readFile('./images/cat.jpeg')
+      console.log('downloading image...')
+      const buffer = await this.getImageBuffer(imageUrl)
+      console.log('Detecting labels...')
+      const {names, workingItems} = await this.detectImageLabels(buffer) 
+      
       console.log('Translating to Portuguese...')
+      const texts = await this.translateText(names)
 
-      const texts = await this.translateText(names) 
-
-      console.log('Formating text...')
+      console.log('handling final object...')
       const finalText = this.formatTextResults(texts, workingItems)
-
+      console.log('finishing...')
       return {
         statusCode: 200,
-        body: `A imagem tem \n`.concat(finalText)
+        body: `A imagem tem\n `.concat(finalText)
       }
     } catch (error) {
-      console.log(error);
+      console.log('Error***', error.stack)
       return {
         statusCode: 500,
-        body: 'Internal server error'
+        body: 'Internal server error!'
       }
     }
   }
 }
 
+//factory
 const aws = require('aws-sdk')
 const reko = new aws.Rekognition()
 const translator = new aws.Translate()
 const handler = new Handler({
-  rekoSvc: reko,
+  rekoSvc : reko,
   translatorSvc: translator
 })
 
-module.exports.main = handler.main.bind(handler);
+module.exports.main = handler.main.bind(handler)
